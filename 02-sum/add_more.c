1778cf044498b2ca75d246f4ccf46a6b7fadc827
Tiago Gomes <tidgomes@gmail.com>
2024-10-11 14:43:13 -0300

    create a program to sum 4 numbers

---
+++ 02-sum/add_more.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int num1, num2, num3, num4, sum;
+
+	printf("Enter the first number: ");
+	scanf("%d", &num1);
+
+	printf("Enter the second number: ");
+	scanf("%d", &num2);
+
+	printf("Enter the third number: ");
+	scanf("%d", &num3);
+
+	printf("Enter the fourth number: ");
+	scanf("%d", &num4);
+
+	sum = num1 + num2 + num3 + num4;
+
+	printf("%d + %d + %d + %d = %d\n", num1, num2, num3, num4, sum);
+	return 0;
+}
