b4454771cee910f46720882acf7b63b6a910a953
Tiago Gomes <tidgomes@gmail.com>
2025-02-17 14:12:11 -0300

    remove unnecessary folders

--- 01-hello-world/hello_tiago.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello Tiago, how are you doing?\n");
-	printf("I am doing well Tiago, how about you?\n");
-	printf("Very well indeed!\n");
-	return 0;
-}
