fd692e7c09cc1c1e22cb710e3f2a27c0b2414e6e
Tiago Gomes <tidgomes@gmail.com>
2025-02-19 17:47:58 -0300

    solve ex3

--- CAP006/ex003/intercalar_vetore.c
+++ CAP006/ex003/intercalar_vetore.c
@@ -3,20 +3,20 @@
 int main() {
 	int vet_1[10], vet_2[10], vet[20], count = 0;
 
-	printf("Preencha o primeiro vetor: ");
+	printf("Preencha o primeiro vetor: \n");
 
 	for (int i = 0; i < 10; i++) {
 		printf("Posição %d: ", i);
 		scanf("%d", &vet_1[i]);
 	}
 
-	printf("\nPreencha o segundo vetor: ");
+	printf("\nPreencha o segundo vetor: \n");
 	for(int j = 0; j < 10; j++) {
 		printf("Posição %d: ", j);
 		scanf("%d", &vet_2[j]);
 	}
 
-	printf("\n\nNovo vetor");
+	printf("\n\nNovo vetor\n");
 	for(int i = 0; i < 20; i += 2) {
 		vet[i] = vet_1[count];
 		vet[i + 1] = vet_2[count];
@@ -24,7 +24,7 @@ int main() {
 	}
 
 	for(int i = 0; i < 20; i++){
-		printf("%d: [%d]", i, vet[i]);
+		printf("%d: [%d]\n", i, vet[i]);
 	}
 	printf("\n");
 	return 0;
