558b23298726f654e8b2b7a400073725e48481f8
Tiago Gomes <tidgomes@gmail.com>
2025-02-19 18:21:11 -0300

    start the exercise 5

---
+++ CAP006/ex005/uniao_produto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int vet_1[10], vet_2[10], count_uniao;
+
+	printf("Preencha o primeiro vetor: \n");
+	for(int i = 0; i < 10; i++) {
+		printf("Posição %d: ");
+		scanf("%d", &vet_1[i]);
+	}
+	printf("\nPreencha o segundo vetor: \n");
+	for(int i = 0; i < 10; i++){
+		printf("Posição %d: ");
+		scanf("%d", &vet_2[i]);
+	}
+
+	for(int i = 0; i < 10; i++) {
+		for(int j = 0; j < 10; j++) {
+			
+	return 0;
+}
