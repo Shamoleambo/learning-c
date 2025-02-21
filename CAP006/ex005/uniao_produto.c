43927809c8f61b456b7e2ebefee1805a783a4090
Tiago Gomes <tidgomes@gmail.com>
2025-02-21 11:56:19 -0300

    terminar parte da uniao

--- CAP006/ex005/uniao_produto.c
+++ CAP006/ex005/uniao_produto.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	int vet_1[10], vet_2[10], count_uniao;
+	int vet_1[10], vet_2[10], uniao[10] = {0};
 
 	printf("Preencha o primeiro vetor: \n");
 	for(int i = 0; i < 10; i++) {
-		printf("Posição %d: ");
+		printf("Posição %d: ", i);
 		scanf("%d", &vet_1[i]);
 	}
 	printf("\nPreencha o segundo vetor: \n");
 	for(int i = 0; i < 10; i++){
-		printf("Posição %d: ");
+		printf("Posição %d: ", i);
 		scanf("%d", &vet_2[i]);
 	}
 
 	for(int i = 0; i < 10; i++) {
 		for(int j = 0; j < 10; j++) {
+			if(vet_1[i] == vet_2[j]) {
+				uniao[i] = vet_2[j];
+			}
+		}
+	}
+
+	printf("\n\n");
+	for(int i = 0; i < 10; i++) {
+		printf("%d\n", uniao[i]);
+	}
+	printf("\n\n");
 			
 	return 0;
 }
