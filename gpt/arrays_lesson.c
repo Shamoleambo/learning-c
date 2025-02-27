2204c39418592390e047e7e291336248537e1334
Tiago Gomes <tidgomes@gmail.com>
2025-02-27 07:41:30 -0300

    learn about realloc

--- gpt/arrays_lesson.c
+++ gpt/arrays_lesson.c
@@ -18,11 +18,36 @@ int main() {
 		scanf("%d", &arr[i]);
 	}
 
-	printf("Array elements: ");
+	printf("Initial array elements: ");
 	for(int i = 0; i < n; i++) {
 		printf("%d", arr[i]);
 	}
 	printf("\n");
+
+	int new_size;
+	printf("Enter the new size of the array: ");
+	scanf("%d", &new_size);
+
+	arr = (int*) realloc(arr, new_size * sizeof(int));
+
+	if(arr == NULL) {
+		printf("Memory realocation failed!\n");
+		return 1;
+	}
+
+	if(new_size > n) {
+		printf("Enter %d more integers:\n", new_size - n);
+		for(int i = n; i < new_size; i++) {
+			scanf("%d", &arr[i]);
+		}
+	}
+
+	printf("Resized array elements: ");
+	for(int i = 0; i < new_size; i++) {
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+
 
 	free(arr);
 	return 0;
