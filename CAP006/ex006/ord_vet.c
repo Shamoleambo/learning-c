09ba6d623fe5144e02e81b0ef7ea78844817c77e
Tiago Gomes <tidgomes@gmail.com>
2025-02-28 07:39:03 -0300

    solve ex 6

--- CAP006/ex006/ord_vet.c
+++ CAP006/ex006/ord_vet.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
 int main () {
-	int vet[10], decresc[10], add, count = 0;
+	int vet[10], add, count = 0;
 
-	printf("Forneça o array: ");
+	printf("Forneça o array: \n");
 	for(int i = 0; i < 10; i++) {
 		printf("Elemento da posição %d: ", i);
 		scanf("%d", &vet[i]);
+	}
+	
+	do {
+		int temp;
+		if(vet[count] < vet[count + 1]) {
+			temp = vet[count];
+			vet[count] = vet[count + 1];
+			vet[count + 1] = temp;
+			count = 0;
+		} else {
+			count ++;
+		}
+	}while(count < 10);
+
+	printf("Vetor em ordem decrescente: \n");
+	for(int i = 0; i < 10; i++) {
+		printf("Posição %d = [%d]\n", i, vet[i]);
 	}
 
 	return 0;
