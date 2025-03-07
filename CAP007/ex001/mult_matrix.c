91dd1de0465550ab62c99cb0593df9b6e1ce2680
Tiago Gomes <tidgomes@gmail.com>
2025-03-07 08:18:15 -0300

    start new chapter

---
+++ CAP007/ex001/mult_matrix.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	/*Fazer um programa que lê uma matriz 2x2
+	 Multiplica esta matriz pelo seu maior elemento
+	 	ou seja: encontrar o maior elemento de uma matriz
+	E exibe a matriz resultante da multiplicação*/
+
+	int mat[2][2]
+	printf("Forneça os dados da matriz: \n\n");
+	
+	for(int i = 0; i < 2; i++) {
+		for(int j = 0; j < 2; j++) {
+			printf("Matriz posição %dx%d: ", i, j);
+			scanf("%d", &mat[i][j]);
+		}
+	}
+
+
+	/*Testando se a matriz está sendo preenchida corretamente: */
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++) {
+			printf("%d  ", mat[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
+}
