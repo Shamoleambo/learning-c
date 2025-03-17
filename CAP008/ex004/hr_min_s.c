4f7196280307eca39d53190980c3b31658e2e5f0
Tiago Gomes <tidgomes@gmail.com>
2025-03-17 18:16:03 -0300

    begin ex4

---
+++ CAP008/ex004/hr_min_s.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*Recebe um valor em segundos e converte para horas, minutos e segundos: HH:mm:ss*/
+
+int main() {
+	int segundos_totais;
+
+	printf("Forneça os segundos totais: ");
+	scanf("%d", &segundos_totais);
+
+	/*In chatgpt there is an explanation on how string work... go look*/
+	return 0;
+}
